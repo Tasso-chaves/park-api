2da0e4a1d689a46ef2c90e04eafbdce88ca2e21b
Tasso-chaves <tassochav@gmail.com>
2023-10-24 18:05:13 -0300

    Cria o Objt de erro para as respostas de requisições

---
+++ src/main/java/com/dev/parkapi/exception/ErrorMessage.java
@@ -0,0 +1,51 @@
+package com.dev.parkapi.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ErrorMessage {
+    
+    private String path;
+    private String method;
+    private int statusCod;
+    private String statusText;
+    private String message;
+    private Map<String, String> errors;
+    
+    public ErrorMessage(){
+
+    }
+    
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.statusCod = status.value();
+        this.message = message;
+
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.statusCod = status.value();
+        this.message = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        this.errors = new HashMap<>();
+        for(FieldError fieldError : result.getFieldErrors()){
+            this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
