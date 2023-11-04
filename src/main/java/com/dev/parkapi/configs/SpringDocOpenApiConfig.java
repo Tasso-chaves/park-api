0aff05222c645bd978371e5af13abf801af8d734
Tasso-chaves <tassochav@gmail.com>
2023-11-04 11:09:51 -0300

    Adiciona e configura, Springdoc-openApi e Swagger-ui

---
+++ src/main/java/com/dev/parkapi/configs/SpringDocOpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.dev.parkapi.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+    
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+            .info(new Info().title("Rest API - Spring Park")
+                            .description("API para gestão de estacionamentos de veiculos")
+                            .version("V1")
+                            .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                            .contact(new Contact().name("Tasso Chaves").email("tassochav@gmail.com")));
+    }
+}
