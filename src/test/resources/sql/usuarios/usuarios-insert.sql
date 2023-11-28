c7be34804295c2aa032f0b80a289e3066a3e76f6
Tasso-chaves <tassochav@gmail.com>
2023-11-27 22:53:11 -0300

    Adiciona teste para criar novo usuario

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,15 @@
+CREATE TABLE USUARIOS(
+    
+ID int,
+CRIADO_POR varchar(255),
+DATA_CRIACAO date,
+DATA_MODIFICACAO date,
+MODIFICADO_POR varchar(255),
+PASSWORD varchar(255),
+ROLE varchar(255),
+USERNAME varchar(255) 
+
+);
 INSERT INTO USUARIOS (id, username, password, role) VALUES (100, 'xas@gmail.com', '123456', 'ROLE_ADMIN');
 INSERT INTO USUARIOS (id, username, password, role) VALUES (101, 'mas@gmail.com', '1234567', 'ROLE_ADMIN');
 INSERT INTO USUARIOS (id, username, password, role) VALUES (102, 'zas@gmail.com', '1234568', 'ROLE_ADMIN');
